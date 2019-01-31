8eb835b9fdd713bb163ebac79d7261413903cf49
Alex Sasser and Sophia Fujiki <alex@>
2019-01-30 20:46:51 -0800

    fix js script issues with having multiple number prompts.

--- js/scripts.js
+++ js/scripts.js
@@ -11,25 +11,18 @@ var subtract = function(number1, number2) {
 return number1 - number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
 
 //Multiplying
 var multiply = function(number1, number2) {
 return number1 * number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
 
 //Dividing
 var divide = function(number1, number2) {
 return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
 
 //Alert to print on page
-alert(add(number1, number2));
+alert(subtract(number1, number2));
