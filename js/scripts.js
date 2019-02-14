a853aa85af1409077d3aabbe1604f2b048dc5cdf
Alex Sasser and John Sweet <alex@>
2019-02-13 20:49:16 -0800

    add whole bunch of js, CSS, HTML stuff.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
+//Business (Back end) Logic
 //Adding
 var add = function(number1, number2) {
 return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
 
 //Subtracting
 var subtract = function(number1, number2) {
@@ -23,6 +22,57 @@ var divide = function(number1, number2) {
 return number1 / number2;
 };
 
+// Front-end Logic
+$(document).ready(function() {
+  $("#add").submit(function(event) {
+    var number1 = parseFloat($("#add1").val());
+    var number2 = parseFloat($("#add2").val());
+    var answer = add(number1, number2);
 
-//Alert to print on page
-alert(subtract(number1, number2));
+    //console.log(answer);
+    //console.log(answer.toFixed(2));
+
+    if(number1 & number2 !== 0) {
+      $("#answer").text(answer.toFixed(2));
+    } else {
+      $("#answer").text(answer);
+    }
+    $("#result").show();
+
+    event.preventDefault();
+  });
+  //make a subtract .submit function HERE
+  $("#subtract").submit(function(event) {
+    var number1 = parseFloat($("#sub1").val());
+    var number2 = parseFloat($("#sub2").val());
+    var answer = subtract(number1, number2);
+    $("#answer").text(answer.toFixed(2));
+    $("#result").show();
+
+    event.preventDefault();
+  });
+  // Multiply function
+  $("#multiply").submit(function(event) {
+    var number1 = parseFloat($("#mult1").val());
+    var number2 = parseFloat($("#mult2").val());
+    var answer = multiply(number1, number2);
+    $("#answer").text(answer.toFixed(2));
+    $("#result").show();
+
+    event.preventDefault();
+  });
+  // Divide function
+  $("#divide").submit(function(event) {
+    var number1 = parseFloat($("#div1").val());
+    var number2 = parseFloat($("#div2").val());
+    var answer = divide(number1, number2);
+    $("#answer").text(answer.toFixed(2));
+    $("#result").show();
+
+    event.preventDefault();
+  });
+});
+  //Alert to print on page
+  // alert(add(number1, number2));
+
+// element.noDefault
