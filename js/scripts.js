78acc5b0a6bc37378c9105c5c7e114a00844682f
Alex Sasser and donavan stokes <donavan@>
2019-02-20 18:36:09 -0800

    change calculator HTML and js to radio button selection of operator.

--- js/scripts.js
+++ js/scripts.js
@@ -24,55 +24,12 @@ return number1 / number2;
 
 // Front-end Logic
 $(document).ready(function() {
-  $("#add").submit(function(event) {
-    var number1 = parseFloat($("#add1").val());
-    var number2 = parseFloat($("#add2").val());
-    var answer = add(number1, number2);
-
-    //console.log(answer);
-    //console.log(answer.toFixed(2));
-
-    if(number1 & number2 !== 0) {
-      $("#answer").text(answer.toFixed(2));
-    } else {
-      $("#answer").text(answer);
-    }
-    $("#result").show();
-
+  $("form#calculator").submit(function() {
     event.preventDefault();
-  });
-  //make a subtract .submit function HERE
-  $("#subtract").submit(function(event) {
-    var number1 = parseFloat($("#sub1").val());
-    var number2 = parseFloat($("#sub2").val());
-    var answer = subtract(number1, number2);
-    $("#answer").text(answer.toFixed(2));
-    $("#result").show();
-
-    event.preventDefault();
-  });
-  // Multiply function
-  $("#multiply").submit(function(event) {
-    var number1 = parseFloat($("#mult1").val());
-    var number2 = parseFloat($("#mult2").val());
-    var answer = multiply(number1, number2);
-    $("#answer").text(answer.toFixed(2));
-    $("#result").show();
-
-    event.preventDefault();
-  });
-  // Divide function
-  $("#divide").submit(function(event) {
-    var number1 = parseFloat($("#div1").val());
-    var number2 = parseFloat($("#div2").val());
-    var answer = divide(number1, number2);
-    $("#answer").text(answer.toFixed(2));
-    $("#result").show();
-
-    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result = add(number1, number2);
+    $("#output").text(result);
   });
 });
-  //Alert to print on page
-  // alert(add(number1, number2));
-
-// element.noDefault
