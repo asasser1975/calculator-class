59026d126209d327df8b32670f048adee4d5264d
Alex Sasser and donavan stokes <alex@>
2019-02-20 19:13:26 -0800

    created new calc app using radio button and branching.

--- js/scripts.js
+++ js/scripts.js
@@ -25,11 +25,21 @@ return number1 / number2;
 // Front-end Logic
 $(document).ready(function() {
   $("form#calculator").submit(function() {
-    event.preventDefault();
     var number1 = parseInt($("#input1").val());
     var number2 = parseInt($("#input2").val());
     var operator = $("input:radio[name=operator]:checked").val();
-    var result = add(number1, number2);
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide"){
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
+
+    event.preventDefault();
   });
 });
